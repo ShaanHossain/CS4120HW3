72b325b9ad839fbce10d545bf6de6c11de8c7ee7
ShaanHossain <shaanhossain19@gmail.com>
2021-10-27 17:29:37 -0400

    Updated model working

--- textclassify_model.py
+++ textclassify_model.py
@@ -1,15 +1,21 @@
-# STEP 1: rename this file to textclassify_model.py
-
-# feel free to include more imports as needed here
-# these are the ones that we used for the base model
 import numpy as np
 import sys
 from collections import Counter
 import math
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 """
 Shaan Hossain
-Your name and file comment here:
+
+This file contains two Naive Baye's classifiers.
+The first classifier uses only BoW for features.
+The second classifier uses several features, pre processing, and text
+normalization.
+There are a variety of reporting functions to compute precision, recall, and F1.
+There are a few helper methods for reporting this data.
+This file contains the k-folds extra credit component.
 """
 
 
@@ -114,6 +120,25 @@ def f1(gold_labels, predicted_labels):
 Implement any other non-required functions here
 """
 
+def k_fold(all_examples, k):
+    # all_examples is a list of tuples of strings formatted [(id, example_text, label), (id, example_text, label)....]
+    # containing all examples from the train and dev sets
+    # return a list of lists containing k sublists where each sublist is one "fold" in the given data
+
+    split_arrays = list(np.array_split(all_examples, k))
+    split_lists = []
+
+    for s in split_arrays:
+
+      tuples = []
+      for t in s:
+        tuples.append(tuple(t))
+
+      split_lists.append(tuples)
+    
+    # print(len(split_lists))
+    return split_lists
+
 def predict_examples(model, examples):
   
   gold_labels = []
@@ -129,6 +154,12 @@ def report_scores(gold_labels, predicted_labels):
   print("Precision: " + str(precision(gold_labels, predicted_labels)))
   print("Recall: " + str(recall(gold_labels, predicted_labels)))
   print("F1-Score: " + str(f1(gold_labels, predicted_labels)))
+
+def flatten_list(lst): 
+  new_list = []
+  for l in lst:
+    new_list += l
+  return new_list
 
 """
 implement your TextClassify class here
@@ -146,7 +177,7 @@ class TextClassify:
     self.class_one_doc_count = 0
     self.class_one_feature_count = 0
     self.total_docs = 0
-    pass
+    self.wordnet_lemmatizer = WordNetLemmatizer() 
 
   def train(self, examples):
     """
@@ -265,46 +296,45 @@ class TextClassify:
 
     return bag_of_words
 
-
   def __str__(self):
     return "Naive Bayes - bag-of-words baseline"
 
 
-class TextClassifyImproved:
+class TextClassifyImproved(TextClassify):
 
-  def __init__(self):
-    pass
+  # def __init__(self):
+  #   pass
 
-  def train(self, examples):
-    """
-    Trains the classifier based on the given examples
-    Parameters:
-      examples - a list of tuples of strings formatted [(id, example_text, label), (id, example_text, label)....]
-    Return: None
-    """
-    pass
+  # def train(self, examples):
+  #   """
+  #   Trains the classifier based on the given examples
+  #   Parameters:
+  #     examples - a list of tuples of strings formatted [(id, example_text, label), (id, example_text, label)....]
+  #   Return: None
+  #   """
+  #   pass
 
-  def score(self, data):
-    """
-    Score a given piece of text
-    you’ll compute e ^ (log(p(c)) + sum(log(p(w_i | c))) here
+  # def score(self, data):
+  #   """
+  #   Score a given piece of text
+  #   you’ll compute e ^ (log(p(c)) + sum(log(p(w_i | c))) here
     
-    Parameters:
-      data - str like "I loved the hotel"
-    Return: dict of class: score mappings
-    return a dictionary of the values of P(data | c)  for each class, 
-    as in section 4.3 of the textbook e.g. {"0": 0.000061, "1": 0.000032}
-    """
-    pass
+  #   Parameters:
+  #     data - str like "I loved the hotel"
+  #   Return: dict of class: score mappings
+  #   return a dictionary of the values of P(data | c)  for each class, 
+  #   as in section 4.3 of the textbook e.g. {"0": 0.000061, "1": 0.000032}
+  #   """
+  #   pass
 
-  def classify(self, data):
-    """
-    Label a given piece of text
-    Parameters:
-      data - str like "I loved the hotel"
-    Return: string class label
-    """
-    pass
+  # def classify(self, data):
+  #   """
+  #   Label a given piece of text
+  #   Parameters:
+  #     data - str like "I loved the hotel"
+  #   Return: string class label
+  #   """
+  #   pass
 
   def featurize(self, data):
     """
@@ -315,10 +345,76 @@ class TextClassifyImproved:
     Return: a list of tuples linking features to values
     for BoW, a list of tuples linking every word to True [("I", True), ("loved", True), ("it", True)]
     """
-    pass
+    
+    features = []
+
+    # tokens = data.split()
+
+    #Modification 1: Normalization: All lowercase
+    #Removing this did not seem to have any performance boost
+    #but it did nothing negative either
+    data = data.lower()
+
+    #Modification 2: Normalization: Tokenizing using NLTK
+    #Keep this
+    # tokens = word_tokenize(data)
+    tokens = data.split()
+
+    #Modification 3: Word List: Removing stop words using NLTK
+    #Keep this
+    stop_words = set(stopwords.words('english'))
+    tokens_filtered = []
+
+    for t in tokens:
+        if t not in stop_words:
+            tokens_filtered.append(t)
+
+    tokens = tokens_filtered
+
+    #Modification 4: Pre-Processing Lemmization using NLTK
+    #Surprisingly does not appear to impact performance
+    # for t in tokens:
+    #   t = self.wordnet_lemmatizer.lemmatize(t)
+
+    capital = 0
+    average_word_length = 5 #It's 4.7, but we'll use 5
+    short_words = 0
+    long_words = 0
+
+    for t in tokens:
+
+      #Feature 1: Bag of words
+      features.append((t, True))
+
+      if(t.isupper()):
+        capital += 1
+
+      #Feature 3: Long or short word counter, intentionally ignoring length 4
+      #and 5 as those are close to average
+      #Very important that stop words were removed
+      if(len(t) > average_word_length):
+        long_words += 1
+      elif(len(t) < average_word_length - 1):
+        short_words += 1
+    
+    #Feature 2: Lots of capital
+    #Remove this. It only appears to be a rough count of sentence number vs.
+    #Capturing any sentiment. Does not impact F1 score in given train/dev sets
+    # if(capital > 2):
+    #   features.append(("LOTS_OF_CAPITAL", True))
+
+    #Feature 3: Long or short words
+    # if(long_words > short_words):
+    #   features.append(("LOTS_OF_LONG_WORDS", True))
+
+
+
+    return features
+
+
 
   def __str__(self):
-    return "NAME OF YOUR CLASSIFIER HERE"
+    return "Naive Bayes - bag-of-words improved with normalization and pre-processing"
 
 
 
@@ -330,27 +426,43 @@ def main():
   training_examples = generate_tuples_from_file(training)
   testing_examples = generate_tuples_from_file(testing)
 
+  # print(training_examples)
+
+  number_of_folds = 10
+
+  folds = k_fold((training_examples + testing_examples), number_of_folds)
+
   classifier = TextClassify()
   print(classifier)
   # do the things that you need to with your base class
-  classifier.train(training_examples)
-  gold_labels, predicted_labels = predict_examples(classifier, testing_examples)
 
-  # report precision, recall, f1
-  report_scores(gold_labels, predicted_labels)
+  for i in range(0, len(folds)):
+    folds_copy = folds.copy()
+    validation = folds_copy.pop(i)
+    training = flatten_list(folds_copy)
 
-  #-------
+    classifier.train(training)
+    gold_labels, predicted_labels = predict_examples(classifier, validation)
 
-  #-------
+    # report precision, recall, f1
+    print("Fold: " + str(i + 1))
+    report_scores(gold_labels, predicted_labels)
 
   improved = TextClassifyImproved()
   print(improved)
   # do the things that you need to with your improved class
 
+  for i in range(0, len(folds)):
+    folds_copy = folds.copy()
+    validation = folds_copy.pop(i)
+    training = flatten_list(folds_copy)
 
-  # report final precision, recall, f1 (for your best model)
+    improved.train(training)
+    gold_labels, predicted_labels = predict_examples(improved, validation)
 
-
+    # report final precision, recall, f1 (for your best model)
+    print("Fold: " + str(i + 1))
+    report_scores(gold_labels, predicted_labels)
 
 
 if __name__ == "__main__":
